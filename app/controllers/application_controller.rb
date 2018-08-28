9d55152b1e4701a49694d9102e8a6f25ad304470
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2018-08-27 20:39:07 -0700

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def hello
-		render html: "hello, cruel world!"
+		render html: "hello, cruel world! \n \n Section 3.4"
 	end 
 end
