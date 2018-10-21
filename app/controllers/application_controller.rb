de2e189f44aaf188e9055fcdfeddf8b6e1a46994
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2018-10-21 15:27:13 -0700

    Finish user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
+	include SessionsHelper
 
-	def hello
-		render html: "hello, cruel world! \n \n Section 3.4"
-	end 
+	# def hello
+	# 	render html: "hello, cruel world! \n \n Section 3.4"
+	# end 
 end
